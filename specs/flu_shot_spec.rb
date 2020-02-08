ad7a418461a5adc8204da20b5d18415b03bd4ecf
Laszlo Papp <nucc@bteam.hu>
2020-02-08 18:28:50 +0000

    Use minitest spec

---
+++ specs/flu_shot_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+describe FluShot do
+  it 'has a VERSION number' do
+    refute_nil ::FluShot::VERSION
+  end
+end
--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -2,3 +2,4 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "flu_shot"
 
 require "minitest/autorun"
+require 'minitest/spec'
